2cef7087ddd396d80d95b121bc2d813336eb300c
bearfall <84324334+bearfall@users.noreply.github.com>
2022-10-29 19:41:41 +0800

    1029

--- Assets/FirstScript.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class FirstScript : MonoBehaviour { }
-    namespace bearfall
-{
-
-}
-
-
---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+
+namespace bearfall
+{
+    public class FirstScript : MonoBehaviour 
+    {
+        private void Awake()
+        {
+            print("hello");
+        }
+
+        private void Start()
+        {
+            print("<color=yellow>start</color>");
+        }
+    }
+}
+
+
