4878fbe9504fcef004538a3038ed69214f1fa6ad
bearfall <84324334+bearfall@users.noreply.github.com>
2022-11-28 17:59:01 +0800

    1128

--- Assets/Scripts/FinalManager.cs
+++ Assets/Scripts/FinalManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
 namespace bearfall
 {
     /// <summary>
@@ -9,6 +11,8 @@ namespace bearfall
 
     public class FinalManager : MonoBehaviour
     {
+
+        public static FinalManager instance;
         /// <summary>
         /// 結束畫布
         /// </summary>
@@ -23,9 +27,42 @@ namespace bearfall
         private Button btnReplay;
         private void Awake()
         {
+            instance = this;
+
             groupFinal = GameObject.Find("結束畫布").GetComponent<CanvasGroup>();
             textTitle = GameObject.Find("結束標題").GetComponent<TextMeshProUGUI>();
             btnReplay = GameObject.Find("重新遊戲").GetComponent<Button>();
+            btnReplay.onClick.AddListener(Replay);
+        }
+        /// <summary>
+        /// 遊戲結束
+        /// </summary>
+        /// <param name="title">遊戲標題</param>
+        public void GameOver(string title)
+        {
+            textTitle.text = title;
+
+            StartCoroutine(FadeIn());
+        }
+
+        /// <summary>
+        /// 淡入
+        /// </summary>
+        private IEnumerator FadeIn()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                groupFinal.alpha += 0.1f;
+                yield return new WaitForSeconds(0.02f);
+            }
+            groupFinal.interactable = true;
+            groupFinal.blocksRaycasts = true;
+            
+        }
+
+        private void Replay()
+        {
+            SceneManager.LoadScene("開始選單");
         }
     }
 }
